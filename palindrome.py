c1b02ca885bdbf3f33d0d196fd42e3c9686ca0d9
Nadia Mounzih <nmounzih@yahoo.com>
2017-02-27 20:55:15 -0500

    Fixed 'alaska' error

--- palindrome.py
+++ palindrome.py
@@ -4,23 +4,20 @@ import re
 def is_palindrome(sentence):
     sentence = re.sub(r'[^A-Za-z]', "", sentence.lower())
     if sentence == "":
-        return
-    if sentence[0] == sentence[-1]:
-        sentence = sentence[1: -1]
-        is_palindrome(sentence)
         return True
-    else:
-        return False
+    if sentence[0] == sentence[-1]:
+        new_sentence = sentence[1: -1]
+        return is_palindrome(new_sentence)
+    return False
+
 
 def main():
     user_input = input("Is it a palindrome? ")
     print(is_palindrome(user_input))
-    if is_palindrome(user_input) == True:
+    if is_palindrome(user_input) is True:
         print("Yes, it is a palindrome.")
     else:
         print("No, it is not a palindrome.")
-
-
 
 
 if __name__ == '__main__':
